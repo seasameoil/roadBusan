672ea4d906a3ea9dce9802db85efc082ab684301
seasameoil <whdwlgus12@gmail.com>
2022-08-11 16:57:39 +0900

    네이버 검색 api 연결 완료

--- server/bar/bar_restaurant.js
+++ server/bar/bar_restaurant.js
@@ -1,78 +1,44 @@
 const express = require("express");
-//const { query, response } = require("express");
-const axios = require("axios");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const b_res = express.Router();
-/*
-var client_id = "CLIENT_ID";
-var client_secret = "CLIENT_SECRET";*/
+const request = require("request");
+const app = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 dotenv.config();
 
-b_res.use(cors());
-/*b_res.use(express.json());
-b_res.use(express.urlencoded({ extended: false }));
-/*
-b_res.post("/restaurant", async (req, res) => {
-  query = req.body.query;
+app.get("/data", async (req, res) => {
+  //파라미터 확인
+  /*const { query } = req.body;
+  console.log(req.body); */
 
-  var api_url =
-    "https://openapi.naver.com/v1/datalab/search?query=" + encodeURI(query);
+  var url =
+    "https://openapi.naver.com/v1/search/blog?display=3&start=1&sort=sim&query=" +
+    encodeURI(req.query.query);
+  //URL 확인
+  //console.log("==============================\n" + url);
+
   var options = {
+    url: url,
     headers: {
-      "X-Naver-Client-Id": client_id,
-      "X-Naver-Clinet-Secret": client_secret,
+      "X-Naver-Client-Id": process.env.CLIENT_ID,
+      "X-Naver-Client-Secret": process.env.CLIENT_SECRET,
     },
   };
 
-  await axios
-    .get(api_url, options)
-    .then((res) => {
-      if (res.status === 200) {
-        items = response.addTrailers.items;
-        items.map((x) => {
-          x.title = x.title.replace(/<b>/g, "");
-          x.title = x.title.replace(/<\/b>/g, "");
-          x.description = x.description.replace(/<b>/g, "");
-          x.description = x.description.replace(/<\/b>/g, "");
-        });
-
-        res.render("result", { items: items });
-      }
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-  res.send("Hello");
-});*/
-
-b_res.get("/data", async (req, res) => {
-  try {
-    const request_body = {
-      startDate: "2017-10-01",
-      endDate: "2020-10-30",
-      timeUnit: "month",
-      keywordGroups: [
-        { groupName: "치킨", keywords: ["BBQ", "BHC", "교촌치킨"] },
-        { groupName: "떡볶이", keywords: ["엽기떡볶이", "신전떡볶이", "배떡"] },
-      ],
-    };
-    const url = "https://openapi.naver.com/v1/datalab/search";
-    const headers = {
-      "Content-Type": "application/json",
-      "X-Naver-Client-Id": process.env.CLIENT_ID,
-      "X-Naver-Client-Secret": process.env.CLIENT_SECRET,
-    };
-    const result = await axios.post(url, request_body, {
-      headers: headers,
-    });
-    console.log(result.data);
-    return res.json(result.data);
-  } catch (error) {
-    console.log(error);
-    return res.json(error);
-  }
+  //request가 현재 deprecated 되었지만 axios 사용방법을 잘몰라 request 사용하기로 결정
+  request.get(options, (err, response, body) => {
+    if (!err && response.statusCode == 200) {
+      res.writeHead(200, { "Context-Type": "text/json;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
 });
 
-module.exports = b_res;
+module.exports = app;
