037ed57102952d9b8608a5974dcfc1ccd97d9eec
wodud4143 <wodud4143@naver.com>
2022-08-14 21:26:29 +0900

    산책로 페이지 생성
    구마다 산책로 페이지 생성
    지도 좌표 설정 및 페이지 이동 생성

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ server.use(express.static("public"));
 const b_res = require("./server/bar/bar_restaurant");
 server.use(b_res);
 
+const w_trail = require("./server/walkingtrail/walkingtrail_path");
+server.use('/walkingtrail',w_trail);
+
 server.listen(3000, () => {
   console.log("listenign at http://localhost:3000");
 });
+
+
