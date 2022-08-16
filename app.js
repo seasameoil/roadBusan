c17ab14bcb641d52d01186a00cd935a79aa8c87a
seasameoil <whdwlgus12@gmail.com>
2022-08-17 00:49:21 +0900

    구별 액티비티 추천

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ server.use(express.static("public"));
 
 const b_res = require("./server/bar/restaurant");
 server.use(b_res);
+const b_activity = require("./server/bar/activity");
+server.use(b_activity);
 
 const w_trail = require("./server/walkingtrail/walkingtrail_path");
 server.use("/walkingtrail", w_trail);
---
+++ server/bar/activity.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const request = require("request");
+const app = express.Router();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+dotenv.config();
+
+app.get("/activity", async (req, res) => {
+  var url =
+    "https://openapi.naver.com/v1/search/local.json?display=3&start=1&sort=random&query=" +
+    encodeURI(req.query.query);
+  //URL 확인
+  console.log("==============================\n" + url);
+
+  var options = {
+    url: url,
+    headers: {
+      "X-Naver-Client-Id": process.env.NAVER_CLIENT_ID,
+      "X-Naver-Client-Secret": process.env.NAVER_CLIENT_SECRET,
+    },
+  };
+
+  request.get(options, (err, response, body) => {
+    if (!err && response.statusCode == 200) {
+      res.writeHead(200, { "Context-Type": "text/json;charset=utf-8" });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log("error = " + response.statusCode);
+    }
+  });
+});
+
+module.exports = app;
