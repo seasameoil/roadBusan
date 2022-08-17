ffd1e7d4e8bf6c8628c998f614b52ac7ec6ba71c
JEONGJAEIK <wodlr1207@naver.com>
2022-08-17 22:41:21 +0900

    코드 정리
    자바스크립트 모두 main.js로
    주석 처리는 설명
    액티비티, 산책로에도 구역 나오게

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,41 @@
-const map = document.querySelector('#mapdiv');
+const map = document.querySelector("#mapdiv");
 
 setTimeout(() => {
-    map.classList.remove('no');
-}, 1500);
+  map.classList.remove("no");
+}, 1200);
+
+$("#logo").animate(
+  {
+    width: "10vw",
+    height: "7vh",
+    marginLeft: "3vw",
+    marginTop: "-8vh",
+  },
+  1500
+);
+
+$(function () {
+  $("#map").rwdImageMaps();
+  $("#map").maphilight({
+    strokeColor: "00b050",
+    strokeWidth: 4,
+    strokeOpacity: 0.4,
+    fillColor: "00b050",
+    fillOpacity: 0.4,
+    singleSelect: true,
+  });
+});
+{
+  /* <script>
+      type="text/javascript"
+      src="//dapi.kakao.com/v2/maps/sdk.js?appkey=	b11350e62dc8426b36de65e17fc7b3b5"
+    </script>
+    <script>
+      var container = document.getElementById("map");
+      var options = {
+        center: new kakao.maps.LatLng(35.17944, 129.07556),
+        level: 3,
+      };
+      var map = new kakao.maps.Map(container, options);
+    </script> 카카오맵 API 주석처리 */
+}
